ad0186dccee1214ab0a1d140704680f43aab992e
Gaia Stucky de Quay <gaiastucky@gmail.com>
2021-06-15 22:59:02 +0100

    Changed plotting, functions, etc.

--- CraterCode.py
+++ CraterCode.py
@@ -1,49 +1,43 @@
-### Code to randomly generatate a number of craters
+### Code to randomly generatate a number of craters on a planetary surface
+## by Emily Bamber & Gaia Stucky de Quay (June, 2021)
 ######################################################
 ## Some Resources??
 ## https://eli.thegreenplace.net/2010/01/22/weighted-random-generation-in-python/
 ## beta, a = 1, b = 10, from here? https://cmdlinetips.com/2018/03/probability-distributions-in-python/#:~:text=There%20are%20at%20least%20two%20ways%20to%20draw,9%20most%20commonly%20used%20probability%20distributions%20using%20SciPy.stats.
 
 import numpy as np
-from landlab import RasterModelGrid, imshow_grid
+from landlab import imshow_grid
 import random as rdm
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from crater_functions import do_cratering
-
-##Set the colourmap
-cmap1 =  mpl.cm.get_cmap("Greys_r").copy().reversed()
-cmap2 = mpl.cm.get_cmap("Spectral").copy().reversed()
+from crater_functions import do_cratering, make_noisy_surface
 
 ### Define some variables for our grid!
 ##################################
-xy = 500 #Set the number of nodes in both x and y space
-spacing = 2
-zfactor = 10 #set factor to multiply random noise by
+size = 1000 #size of study area in km (x or y length)
+spacing = 5 #size of cells in km
+zfactor = 1 #set factor to multiply random noise by
 
 ## Set up a LandLab model grid object, with random topographic noise on the order of "zfactor"
 ##################################
-mg = RasterModelGrid((xy,xy), xy_spacing = spacing); #see above for variables
-z = mg.add_zeros('topographic__elevation', at='node') #create an array of zeros for each node of the model grid
-#### Add noise to the surface
-rf = 0.1 #randomness factor (how rough initial terrain is)
-np.random.seed(30) # Keep this constant (e.g., at 30) so the initial randomness it always the same
-noise = (np.random.rand(mg.number_of_nodes)) #between 0 and 1
-z += (noise) * rf # make the noise large enough relative to crater
-mg.at_node["topographic__elevation"] *= zfactor 
-
-# Show the initial, noisy topography:
-##################################
-plt.figure() #show the model grid, values of elevation
-imshow_grid(mg, 'topographic__elevation', plot_name= 'Initial topographic surface', cmap=cmap2, colorbar_label='Elevation [m]')
+xy = int(size/spacing) # number of nodes
+mg = make_noisy_surface(xy,spacing,rf=1) # rf is randomness factor
+cmap2 = mpl.cm.get_cmap("Spectral").copy().reversed() # pastel blue-red for topography
+fig, ax = plt.subplots() #initiate figure
+imshow_grid(mg, 'topographic__elevation', cmap=cmap2, colorbar_label='Elevation [m?????????]')
+plt.title("Initial topographic surface")
+plt.xlabel("X [km]"); plt.ylabel("Y [km]")
+plt.show()
 
 ## Set the "distribution" as of crater size as weights, and choose a value from those weights
 ##################################
 ###  These parameters describe the population frequency for crater diameters:
 Kx = 1.0    #Scaling coefficient (Howard, 2007)
 delta = 2.0 #km, scaling exponent (Howard, 2007)
-minD = spacing * 3 # want it to be greater than spacing, three times the smallest cell
-maxD = int(xy/4) #max diameter (m), a quarter of the total domain width
+minD = int(spacing) * 3 # want it to be greater than spacing, three times the smallest cell
+print("Minimum possible crater size:", minD, "km")
+maxD = int((size)/4) #max diameter (m), a quarter of the total domain width
+print("Maximum possible crater size:", maxD, "km")
 NDs = []
 for D in range(minD, maxD):
     ND = Kx * D **-delta
@@ -51,24 +45,28 @@ for D in range(minD, maxD):
 
 ## Blast the surface with craters!
 ##################################
-#Ncraters = 201 #Number of craters to add
-Ncraters = int(input("How many craters do you want to add? ")) # see console to add your own number!
+Ncraters = 200 #Number of craters to add
+#Ncraters = int(input("How many craters do you want to add? ")) # see console to add your own number!
 count = 0
 rdm.seed(50) #Chose random seed number 50 (this ensures crater locations are same every time)
 mg = do_cratering(Ncraters, NDs, minD, maxD, xy, mg, spacing)
 
-
 ## Make figure of final cratered surface:
 ##################################
+##Set the colourmap
+cmap1 =  mpl.cm.get_cmap("Greys_r").copy().reversed() # grey for hillshade
+
+#Edit data for improved plotting
 hs = mg.calc_hillshade_at_node(elevs='topographic__elevation') #create hillshade file
 topo = mg.field_values('node', 'topographic__elevation').reshape((xy, xy))
 hill = np.reshape(hs, (xy, xy))
 
 fig, ax = plt.subplots() #initiate figure
-img1 = plt.imshow(hill, cmap=cmap1, alpha=1)
-img2 = plt.imshow(topo, cmap=cmap2, alpha=0.6)
-fig.colorbar(img2,ax=ax, label="Elevation [m]")
+img1 = plt.imshow(hill, cmap=cmap1, alpha=1, extent = [0,size, 0, size])
+img2 = plt.imshow(topo, cmap=cmap2, alpha=0.6, extent = [0,size, 0, size])
+fig.colorbar(img2,ax=ax, label="Elevation [m???????????]")
 plt.title("Final cratered surface")
+plt.xlabel("X [km]"); plt.ylabel("Y [km]")
 plt.show()
 
     
--- make_frames.py
+++ make_frames.py
@@ -2,8 +2,7 @@
 
 import os
 import numpy as np
-from landlab import RasterModelGrid, NodeStatus, values
-from crater_functions import do_cratering
+from crater_functions import do_cratering, make_noisy_surface
 import matplotlib.pyplot as plt
 
 # fix random seed
@@ -15,31 +14,22 @@ cmap1 = "Greys" # (Greys, as is - i.e. not reversed)
 cmap2 = "Spectral_r" # (Spectral, reversed)
 
 # Define some Variables!
-xy = 200  # Set the number of nodes in both x and y space
-spacing = 1
-zfactor = 10  # set factor to multiply random noise by
+size = 1000
+spacing = 20
 
 # Set up a LandLab model grid object, random topographic noise ~O(zfactor)
-mg = RasterModelGrid((xy, xy), xy_spacing=spacing)  # see above for variables
-# create an array of zeros for each node of the model grid
-z = mg.add_zeros('topographic__elevation', at='node')
-# add noise to the surface
-noise = values.random(mg, "topographic__elevation", at='node',
-                      where=NodeStatus.CORE, distribution='uniform',
-                      high=z.max()*0.75, low=z.max()*0.25)
-mg.at_node["topographic__elevation"] *= zfactor
+xy = int(size/spacing)
+mg = make_noisy_surface(xy,spacing,rf=1) # rf is randomness factor
 
 # Set the "distribution" as weights, and choose a value from those weights
 Kx = 1.0  # Scaling coefficient
 delta = 2.0  # km, scaling exponent
-#Ncraters = 100  # Number of craters to add
-Ncraters = int(input("How many craters do you want to add? ")) # see console to add your own number!
-# int(spacing*3) # min diameter (m), xy_spacing times 3
-# i.e. more tha 3 cells in diameter
-minD = 1
-# int((mg.number_of_nodes)/4) #max diameter (m)
-# i,e, a quarter of the total domain width
-maxD = 100
+Ncraters = 2 # Number of craters to add
+#Ncraters = int(input("How many craters do you want to add per timestep? ")) # see console to add your own number!
+minD = int(spacing * 3) # want it to be greater than spacing, three times the smallest cell
+print("Minimum possible crater size:", minD, "km")
+maxD = int((size)/4) #max diameter (m), a quarter of the total domain width
+print("Maximum possible crater size:", maxD, "km")
 NDs = []
 for D in range(minD, maxD):
     ND = Kx * D ** - delta
@@ -49,45 +39,18 @@ for D in range(minD, maxD):
 if os.path.isdir('figs') is False:
     os.mkdir('figs')
 
-# define where section is taken along y
-stk = 100
-
-# number of timesteps
+# Define where section is taken along y (in km)
+stk_km = 500
+stk = int(stk_km/spacing) #node location of stk_km
+# Number of timesteps
 nsteps = 10
 
 # set vertical limits of plots (should be based on last timestep elevations)
 zmin = -100
 zmax = 20
 
-# loop to make time-series frames
-fig, ax = plt.subplots(1, 2, dpi=250, facecolor='w', figsize=(8, 3))
-topo = mg.field_values('node', 'topographic__elevation').reshape((xy, xy)) #create an array of elevation values across the mg domain
-hs = mg.calc_hillshade_at_node(elevs='topographic__elevation') #create a hillshade array
-hill = np.reshape(hs, (xy, xy)) #reshape the hillshade array to be the same shape as the topo array
-img1 = ax[0].imshow(hill, cmap=cmap1, alpha=1, vmin=zmin, vmax=zmax)
-img2 = ax[0].imshow(topo, cmap=cmap2, alpha=0.6, vmin=zmin, vmax=zmax)
-ax[0].set_title('Topography overlain on Hillshade')
-cbar = plt.colorbar(img2, fraction=0.045, ax=ax[0], label = "Topography [m]")
-cbar.set_label('Elevation [m]')
-ax[0].set_xlabel('X')
-ax[0].set_ylabel('Y')
-ax[0].plot(np.linspace(0, xy), np.ones_like(np.linspace(0, xy))*stk,
-           c='k', linestyle='--')
-ax[0].set_xlim([0, xy])
-ax[0].set_ylim([0, xy])
 
-ax[1].plot(topo[stk, :], c=[0, 0, 0])
-ax[1].set_title('Topographic Section at Y = ' + str(stk))
-ax[1].set_ylabel('Topography [m]')
-ax[1].set_xlabel('Distance along X')
-ax[1].set_ylim([zmin, zmax])
-
-plt.tight_layout()
-plt.savefig('figs/' + '0'.zfill(4) + '.png', bbox_inches='tight')
-
-# store old topo sections
 old_arr = np.zeros((nsteps, xy))
-old_arr[0, :] = topo[:, stk]
 
 for i in range(1, nsteps):
     # crater landscape
@@ -98,24 +61,24 @@ for i in range(1, nsteps):
     topo = mg.field_values('node', 'topographic__elevation').reshape((xy, xy)) #create an array of elevation values across the mg domain
     hs = mg.calc_hillshade_at_node(elevs='topographic__elevation') #create a hillshade array
     hill = np.reshape(hs, (xy, xy)) #reshape the hillshade array to be the same shape as the topo array
-    img1 = ax[0].imshow(hill, cmap=cmap1, alpha=1, vmin=0, vmax=1)
-    img2 = ax[0].imshow(topo, cmap=cmap2, alpha=0.6, vmin=zmin, vmax=zmax)
+    img1 = ax[0].imshow(hill, cmap=cmap1, alpha=1, extent = [0,size, 0, size], vmin=0, vmax=1)
+    img2 = ax[0].imshow(topo, cmap=cmap2, alpha=0.6, extent = [0,size, 0, size], vmin=zmin, vmax=zmax)
     ax[0].set_title("Cratered surface evolution (t = %i)" %i)
-    cbar = plt.colorbar(img2, fraction=0.045, ax=ax[0], label = "Topography [m]")
-    cbar.set_label('Elevation [m]')
-    ax[0].set_xlabel('X')
-    ax[0].set_ylabel('Y')
-    ax[0].plot(np.linspace(0, xy), np.ones_like(np.linspace(0, xy))*stk,
+    cbar = plt.colorbar(img2, fraction=0.045, ax=ax[0], label = "Elevation [m??????]")
+    ax[0].set_xlabel('X [km]')
+    ax[0].set_ylabel('Y [km] ')
+    #stk = int(stk_km/spacing)
+    ax[0].plot(np.linspace(0, size), np.ones_like(np.linspace(0, size))*stk_km,
                c='k', linestyle='--')
-    ax[0].set_xlim([0, xy])
-    ax[0].set_ylim([0, xy])
+    ax[0].set_xlim([0, size])
+    ax[0].set_ylim([0, size])
 
     ax[1].plot(topo[stk, :], c=[0, 0, 0], zorder=10)
     for j in range(old_arr.shape[0]):
         if np.sum(old_arr[j, :]) != 0:
             ax[1].plot(old_arr[j, :], c=[1-j/nsteps, 1-j/nsteps, 1-j/nsteps])
-    ax[1].set_title('Topographic section at Y = ' + str(stk))
-    ax[1].set_ylabel('Topography [m]')
+    ax[1].set_title('Topographic section at Y = ' + str(stk_km) + " km")
+    ax[1].set_ylabel('Elevation [m???]')
     ax[1].set_xlabel('Distance along X')
     ax[1].set_ylim() # I think making a dynamic scale is kind of fun, shows how much it changes? Instead of [zmin, zmax]
 
