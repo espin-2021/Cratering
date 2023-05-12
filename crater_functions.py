2d601c00ec08cd73232a20dc7ee3175acd968b1e
Emily Bamber <emily.bamber@utexas.edu>
2023-05-12 04:04:52 -0500

    added A. Moodie's CSFD functions to crater_functions.py

--- crater_functions.py
+++ crater_functions.py
@@ -14,7 +14,6 @@ import craterstats as cst
 from landlab import RasterModelGrid
 from landlab import imshow_grid
 
-
 rn_gen = np.random.default_rng(seed=3);
 
 def weighted_choice_sub(weights):
@@ -135,9 +134,148 @@ def crater_depth(mg, d, diameter, d_ref=7, rim = True):
      
     return mg
 
-def do_cratering(mg, grid_size, cell_size, Ncraters, NDs, minD, maxD, rim = True):
+
+
+## for crater_production_function_inverese and generate_csfd_from_production_function
+## load and format paths for craterstats functions
+functions_path = os.path.join(cst.PATH, "config/functions.txt")
+craterstats_functions = cst.gm.read_textstructure(functions_path)
+
+def crater_production_function_inverse(pf, irange, y, tol=1e-6):
+    """Inverse of the crater production function.
+    This function is modified from Andrew Moodie's "CraterModel" code
+
+    Parameters
+    ----------
+    pf :
+        Production function
+
+    irange
+        Range of interest
+        
+    y : ?
+        ?
+    
+    tol : ?
+        ?
+        
+    Returns
+    ----------
+    ? : ?
+        ?
     """
-    Add craters to some landlab raster model.
+    log_y = np.log10(y)
+    ## xrange = np.log10(pf.range)
+    xrange = np.log10(irange)
+    divisions = 9
+    count = 0
+
+    while True:
+        x0 = np.linspace(xrange[1], xrange[0], divisions)  # reverse order because np.interp() requires increasing 'x-values'
+        y0 = np.log10(pf.evaluate("cumulative", 10.0**x0))
+        x = np.interp(log_y, y0, x0)
+        y1 = np.log10(pf.evaluate("cumulative", 10.0**x))
+        q = np.searchsorted(y0, log_y)
+
+        xrange = x0[[q, q - 1]]
+        count += 1
+        if abs(y1 - log_y) < tol or count > 99:
+            break
+
+    return 10**x
+
+
+def generate_CSFD_from_production_function(
+    time_interval,
+    size_interval,
+    domain_area,
+    cell_size,
+    poisson_intervals=True):
+    """
+    Generate a Crater Size Frequency Distribution (CSFD) using Poisson space events.
+    This function is modified from Andrew Moodie's "CraterModel" code
+
+    Parameters
+    ----------
+    time_interval
+        2-element list of start time and end time in Ga (billion years ago), e.g. [4.5, 3.0]
+
+    size_interval
+        2-element list of smallest and largest diameter craters to generate in km.
+
+    domain_area : int
+        domain area in km2.
+        
+    cell_size : int
+        size of 1 cell, in km
+
+    poisson_intervals
+        Use Poisson spaced events? (otherwise just expectation interval).
+        
+    Returns
+    ---------
+    list_d : list
+        list of crater diameters generated in the specified time interval, for the specified domain area size.
+    """
+
+    # production and chronology functions from "craterstats"
+    production_function = cst.Productionfn(craterstats_functions, "Mars, Ivanov (2001)")
+    chronology_function = cst.Chronologyfn(craterstats_functions, "Mars, Hartmann & Neukum (2001)")
+
+    Area = domain_area  # modelled area in km2
+    poisson_intervals = poisson_intervals  #
+
+    list_d = []  # initiate a list for the diameters
+    ## list_dt = []  # list of interarrival times
+
+    diameter_range = size_interval  # in km
+    
+    if diameter_range == None:
+        diameter_range = [cell_size, production_function.range[1]]; 
+        # if no diameter range was input, the diameter range is from the cell size (smallest) 
+        # to whatever the largest given by the producton function is (largest), i.e. what the production function was calibrated over
+        # production_function.range[0] is 10s of m (10 - 50 m depending on the model used), which is probably much smaller than cell size for my models
+    
+    else: 
+        if (diameter_range[0] < production_function.range[0]) or (diameter_range[1] > production_function.range[1]):
+            warnings.warn("Crater range of interest was outside of production functions's defined range. Extrapolating.") 
+
+    N = production_function.evaluate("cumulative", [1.0, diameter_range[0], diameter_range[1]])  # default a0
+    N1_ratio = N[1] / N[0]
+
+    t = time_interval[1]
+    i = 0
+
+
+    while t <= time_interval[0]:
+        # generate crater diameter
+        u = rn_gen.uniform(0, 1);
+        y = u * (N[1] - N[2]) + N[2]
+        d = crater_production_function_inverse(production_function, diameter_range, y)
+
+        # time interval
+        phi = chronology_function.phi(t)
+        lam = phi * N1_ratio * Area
+        if poisson_intervals:
+            u = rn_gen.uniform(0, 1);
+            dt = -np.log(u) / lam  # proper poisson interval
+        else:
+            dt = 1.0 / lam  # mean interval
+
+        list_d.append(d)
+        # list_dt.append(dt)
+
+        t += dt
+        i += 1
+
+    return list_d
+
+
+def add_craters1(mg, grid_size, cell_size, Ncraters, NDs, minD, maxD, rim = True):
+    """
+    Add craters to some landlab raster model, using functions "weights" and "crater_depth" 
+    NOTE: The function "add_craters2" is a method which adds a more realistic ditribution of craters
+    ("add_craters2" uses functions created by Andrew Moodie which draw diameters from a CSFD).
 
     Parameters
     ----------
@@ -185,6 +323,69 @@ def do_cratering(mg, grid_size, cell_size, Ncraters, NDs, minD, maxD, rim = True
         crater_depth(mg, d, diameter, d_ref=7, rim = rim)
 
     return mg
+
+def add_craters2(mg, time_interval, size_interval, grid_size, cell_size,
+    poisson_intervals=True, rim = True):
+    """
+    Add craters to a pre-defined landlab raster grid model.
+    'add_craters2' USES TWO FUNCTIONS CREATED BY ANDREW MOODIE BASED ON CSFDs and "craterstats": 
+    i.e., the functions "generate_CSFD_from_production_function" and "crater_production_function_inverse" (see above)
+    
+    The old function, 'add_craters1' uses functions written by Emily Bamber, 
+    and produces a crater population that's less realistic
+    
+    Parameters
+    ----------
+    mg : landlab.grid.raster.RasterModelGrid
+        Landlab raster model grid of the landscape
+    
+    time_interval : 2-element list
+        2-element list of start time and end time in Ga.
+
+    size_interval : 2-element list
+        2-element list of smallest and largest diameter craters to generate in km.
+
+    grid size : integer
+        Size of the domain, in km 
+        (note: domain is square so length = width)
+    
+    cell_size : integer
+        Size of each cell in km.
+
+    domain_area
+        domain area in km2.
+
+    poisson_intervals
+        Use Poisson spaced events? (otherwise just expectation interval).
+
+        
+    rim : boolean, default = True
+        whether the crater generated has a crater rim or not.
+        argument to be passed to function "crater_depth"
+        
+    Returns
+    -------
+    mg : landlab.grid.raster.RasterModelGrid
+        Landlab raster model grid after craters have modified the topography
+
+    """
+    domain_area = grid_size * grid_size;
+    xy = int (grid_size / cell_size);
+    
+    diameter_list = generate_CSFD_from_production_function(time_interval, size_interval, domain_area, cell_size,
+                                                           poisson_intervals=poisson_intervals);
+    
+    Ncraters = len(diameter_list);
+    
+    for i in range(Ncraters):  # For N number of craters
+        diameter = diameter_list[i]; #select the diameter
+        cratercenter = (rn_gen.integers(1, grid_size, endpoint = True), rn_gen.integers(1, grid_size, endpoint = True));
+        d = mg.calc_distances_of_nodes_to_point(cratercenter); #print(d)
+        
+        crater_depth(mg, d, diameter, d_ref=7, rim = rim); 
+    
+    return mg
+
 
 def central_crater(mg, diameter, grid_size, cell_size, rim = True):
 	"""
@@ -292,3 +493,6 @@ def plot_grid(mg, grid_size, cell_size, Title='Title'):
     plt.title(Title); #add a title
     plt.xlabel("X [km]"); plt.ylabel("Y [km]") #add x and y axis labels
     plt.show() #show the figure
+    
+    
+    
--- scratch.py
+++ scratch.py
@@ -12,46 +12,41 @@ grid_size = 1000 #grid length distance in km
 cell_size = 5 #size of a cell, (km)
 N_craters = 500 # number of craters to add
 cc_d = 400 # diameter of the central crater ('cc'), (km)
-minD = 15
-maxD = 200
+minD = 5
+maxD = 300
 Kx = 1.0    #Scaling coefficient (Howard, 2007)
 delta = 2.0 #km, scaling exponent (Howard, 2007)
 
 #### Plot the rough background:
+###############################
 mg = make_noisy_surface(grid_size, cell_size, rf=1);
 # plot_topo_profile(mg, xy, Title = 'a');
 # plot_grid(mg, grid_size, cell_size, Title='1.1');
 
-## Add craters to the background surface: 
+
+## Add Craters:
+###############
+# ## Add craters to the background surface (new method): 
+# time_interval = [4.0, 1.9]
+# size_interval = [minD, maxD]
+# poisson_intervals = True;     
+
+# mg = add_craters2(mg, time_interval, size_interval, grid_size, cell_size, 
+#                   poisson_intervals=True, rim = True);
+# plot_grid(mg, grid_size, cell_size, Title='1.2');
+
+
+# ## Add craters to the background surface (old method): 
 NDs = []
 for D in range(minD, maxD):
     ND = Kx * D **-delta;
     NDs.append(ND);
 
-mg = do_cratering(mg, grid_size, cell_size, N_craters, NDs, minD, maxD, rim = True);
-# plot_topo_profile(mg, xy, Title = 'b');
-plot_grid(mg, grid_size, cell_size, Title='1.2');
-
+mg2 = add_craters1(mg, grid_size, cell_size, N_craters, NDs, minD, maxD, rim = True)
+plot_grid(mg2, grid_size, cell_size, Title='1.3');
+    
 # ## Add a central crater:
+###########################
 # mg = central_crater(mg, 250, grid_size, cell_size, rim = True);
 # plot_grid(mg, grid_size, cell_size, Title='1.3');
 
-
-### Testing the rimless crater code:
-mg2 = make_noisy_surface(grid_size, cell_size, rf=1);
-# plot_topo_profile(mg, xy, Title = 'a');
-# plot_grid(mg2, grid_size, cell_size, Title='2.1');
-
-## Add craters to the background surface: 
-NDs = []
-for D in range(minD, maxD):
-    ND = Kx * D **-delta
-    NDs.append(ND)
-    
-mg2 = do_cratering(mg2, grid_size, cell_size, N_craters, NDs, minD, maxD, rim = True);
-# plot_topo_profile(mg, xy, Title = 'b');
-plot_grid(mg2, grid_size, cell_size, Title='2.2');
-
-# # ## Add a central crater:
-# # mg2 = central_crater(mg2, 250, grid_size, cell_size, rim = False);
-# # plot_grid(mg2, grid_size, cell_size, Title='2.3');
